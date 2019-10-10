d20b81696d8b5b0e719cac7ea8283eac623b49a7
Brian Yeager <rbrianyeager@gmail.com>
2019-10-10 16:57:20 -0400

    final version

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -29,7 +29,7 @@ function queryAllItems() {
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + "$" + res[i].price);
+      console.log(res[i].item_id + " | " + res[i].product_name + " | " + "$" + res[i].price + " | " + res[i].stock_quantity);
     }
     console.log("-----------------------------------");
     chooseItem();
@@ -61,31 +61,18 @@ function queryAllItems() {
           // console.log('The user choice =', answer.choice);
           // console.log('The user choice =', answer.quantity);
 
-          var chosenItem = answer.choice;
+          var chosenItem = answer.choice - 1;
           var amountChosen = answer.quantity;
           // Checks if the amount of items available for purchase
           if (amountChosen >
             res[chosenItem].stock_quantity) {
-            console.log("We do not have enough " + res[chosenItem - 1].product_name + "'s in stock.  Please choose another amount.")
+            console.log("We do not have enough " + res[chosenItem].product_name + "'s in stock.  Please choose another amount.")
           }
           // If enought items are available thanks the customer and gives them their total
           else {
-            console.log("Thank you for your purchase.  You're total is $" + amountChosen * res[chosenItem - 1].price);
-            updateQuantity();
-            function updateQuantity() {
-              console.log("Updating all " + res[chosenItem - 1].product_name + " quantities...\n");
-              var newQuantity = res[4].stock_quantity - answer.quantity;
-              var query = connection.query(
-                "UPDATE products SET ? WHERE ?",
-                [
-                  // need to update the subtract the amount ordered from the stock quantity with a math equation
-                  { stock_quantity: newQuantity },
-                  { item_id: chosenItem.item_id }
-                ],
-                function (err, res) {
-                  if (err) throw err;
-                })
-            }
+            console.log("Thank you for your purchase.  You're total is $" + amountChosen * res[chosenItem].price);
+            updateQuantity(res[chosenItem], amountChosen);
+            
           }
 
 
@@ -94,6 +81,20 @@ function queryAllItems() {
 
     }
   })
+}
+function updateQuantity(item, amountChosen) {
+  console.log("Updating all " + item.product_name + " quantities...\n");
+  var newQuantity = item.stock_quantity - amountChosen;
+  var query = connection.query(
+    "UPDATE products SET ? WHERE ?",
+    [
+      // need to update the subtract the amount ordered from the stock quantity with a math equation
+      { stock_quantity: newQuantity },
+      { item_id: item.item_id }
+    ],
+    function (err, res) {
+      if (err) throw err;
+    })
 }
 function repeat() {
   inquirer.prompt({
